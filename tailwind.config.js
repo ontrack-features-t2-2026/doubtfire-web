8bdc27f602815d500c8956fa6214165c8b5abd58
maplefoxgit <s223932052@deakin.edu.au>
2026-08-20 07:27:58 +1000

    refactor: keep footer migration scoped

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,6 @@ module.exports = {
   important: true,
   theme: {
     extend: {
-      screens: {
-        'fl-sm': '600px',
-        'fl-md': '960px',
-        'fl-lg': '1280px',
-        'fl-xl': '1920px',
-      },
       colors: {
         'formatif-blue': '#3939ff',
         'formatif-blue-lighter': '#e7e7ff',
